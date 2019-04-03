4f47304219284ea233f0f92daabb38dbf233cc06
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-03 16:17:28 -0500

    Created all fetch calls needed for modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-console.log("hello")
+import apiManager from "./apiManager.js";
+
+let testObject = {
+    email: "user@example.com",
+      user: "username",
+      name: "First Last"
+}
+
+// apiManager.getAll("users").then(usersArray => {
+//             console.log(usersArray)
+//         })
+
+// apiManager.getOne("users", 2).then(user =>{
+//     console.log(user)
+// });
+
+
+apiManager.postAll("users", testObject)
