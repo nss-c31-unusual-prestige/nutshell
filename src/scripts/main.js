ae72feefcdfcc0ebbf6f9000ed407853fbc293aa
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-04 11:20:10 -0500

    Added comments to code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 import apiManager from "./apiManager.js";
 import newsFeedDOM from "./newsFeed/newsDomManage.js"
 
+newsFeedDOM.listAllNews()
--- src/scripts/newsFeed/newsDomManage.js
+++ src/scripts/newsFeed/newsDomManage.js
@@ -1,9 +1,10 @@
 import apiManager from "../apiManager.js"
 import HtmlBuilder from "../HtmlBuilder.js"
-
+// container for appending articles to DOM.
 let articleContainer = document.getElementById("articles-section")
 
 export default {
+    // Function to list all News articles to DOM
     listAllNews(){
         apiManager.getAll("articles")
         .then(articles => articles.forEach(article => {
