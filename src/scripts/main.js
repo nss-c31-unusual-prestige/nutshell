3c375b48c7577ba983fa7bfebc55951a088028f8
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-04 11:35:03 -0500

    completed post to dom function, made json.message.example

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,1 @@
 import messageManager from "./messages.js"
-
-//calls messageManger function
-messageManager.createMessages("messages");
-
-// apiManager.delFetch("users", 2)
--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -1,24 +1,25 @@
 import apiManager from "./apiManager";
 import HtmlBuilder from "./HtmlBuilder";
 
+//reference to document fragment to which new elements will be appended.
+const messageFragment = document.createDocumentFragment();
+//reference to the section of the DOM the fragment will be appended to after all the messages have been built.
+const messageSection = document.getElementById("messages-section");
+
 export default {
     //display current messages.
-    createMessages: (key) => {
+    printMessages: (key) => {
         //calls the API manager to pull the messages from the database,
         apiManager.getAll(key).then(messages => {
             //then for each message, pass the values contained in the database to build the message.
             messages.forEach(message => {
                 //(elementType, elementId, elementTextContent, elementValue)
-                console.log(HtmlBuilder.elementBuilder("P", message.id, message.message, undefined));
+                const element = HtmlBuilder.elementBuilder("P", `message--${message.id}`, message.message, undefined);
+                //adds element to the fragment.
+                messageFragment.appendChild(element);
             })
+            messageSection.appendChild(messageFragment);
+            console.log(messageSection);
         })
     }
-
-
-    //api call to get current messages.
-
-    //build element.
-
-    //post messages.
-
 }
