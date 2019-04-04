0db44d3e4a1713a16445f62f68ffb7802c1b6f09
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-04 13:42:22 -0500

    Almost finished with the form for news

--- src/scripts/newsFeed/newsForm.js
+++ src/scripts/newsFeed/newsForm.js
@@ -6,10 +6,14 @@ let articleContainer = document.getElementById("articles-section")
 export default {
     newsForm(){
         let newsDocFragment = document.createDocumentFragment()
-
-        newsDocFragment.appendChild(HtmlBuilder.elementBuilder("label", undefined, "Title: ", "newsTitle"))
-        newsDocFragment.appendChild(HtmlBuilder.elementBuilder("input", "newsTitle", "Title", "newsTitle"))
-
+        let newsArticle = document.createElement("article")
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("label", undefined, "Title: ", "newsTitle"))
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("input", "newsTitle", "Title", "newsTitle"))
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("label", undefined, "URL: ", "newsUrl"))
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("input", "newsUrl", "URL", "URL"))
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("label", undefined, "Summary: ", "newsSummary"))
+        newsArticle.appendChild(HtmlBuilder.elementBuilder("input", "newsSummary", "newsSummary", "Summary"))
+newsDocFragment.appendChild(newsArticle)
 articleContainer.appendChild(newsDocFragment)
     }
 }
