b17c7f9ea7e9115a8e28044db41b2c69194b5c3c
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-08 18:46:51 -0500

    session storage created

--- src/scripts/Homepage/homePage.js
+++ src/scripts/Homepage/homePage.js
@@ -3,27 +3,33 @@ import homeEvent from "./homeEventListener.js"
 import login from "../sessionStorage/login.js";
 
 let mainArticle = document.getElementById("home-section")
+let currentUser = sessionStorage.getItem("user");
 
 export default {
-    homePage(){
+    homePage() {
         let homePageArticle = htmlBuilder.elementBuilder("article", "homePageArticle")
         homePageArticle.appendChild(htmlBuilder.elementBuilder("h1", "homeTitle", "Unusual Homepage"))
-        homePageArticle.appendChild(htmlBuilder.elementBuilder("label", undefined, "Username"))
-        homePageArticle.appendChild(htmlBuilder.elementBuilder("input", "Username", "User"))
-        homePageArticle.appendChild(htmlBuilder.elementBuilder("label", undefined, "Email"))
-        homePageArticle.appendChild(htmlBuilder.elementBuilder("input", "Email", "Email"))
+        if (currentUser) {
+            homePageArticle.appendChild(htmlBuilder.elementBuilder("h2", `Welcome ${currentUser.name}`));
+        } else {
+            homePageArticle.appendChild(htmlBuilder.elementBuilder("label", undefined, "Username"))
+            homePageArticle.appendChild(htmlBuilder.elementBuilder("input", "Username", "User"))
+            homePageArticle.appendChild(htmlBuilder.elementBuilder("label", undefined, "Email"))
+            homePageArticle.appendChild(htmlBuilder.elementBuilder("input", "Email", "Email"))
 
-        let userLoginButton = htmlBuilder.elementBuilder("button", "userLogin", "Login")
-        //login session storage handler.
-        userLoginButton.addEventListener("click", event => {
-            event.preventDefault();
-            login.storeLogin();
-        })
-        homePageArticle.appendChild(userLoginButton)
-        let registerButton = htmlBuilder.elementBuilder("button", "registerUser", "Register")
-        registerButton.addEventListener("click", homeEvent.registerForm)
-        homePageArticle.appendChild(registerButton)
-
+            let userLoginButton = htmlBuilder.elementBuilder("button", "userLogin", "Login")
+            //login session storage handler.
+            userLoginButton.addEventListener("click", event => {
+                event.preventDefault();
+                const username = document.getElementById("Username").value;
+                const email = document.getElementById("Email").value
+                login.storeLogin(username, email);
+            })
+            homePageArticle.appendChild(userLoginButton)
+            let registerButton = htmlBuilder.elementBuilder("button", "registerUser", "Register")
+            registerButton.addEventListener("click", homeEvent.registerForm)
+            homePageArticle.appendChild(registerButton)
+        }
         mainArticle.appendChild(homePageArticle)
     }
 }
--- src/scripts/sessionStorage/login.js
+++ src/scripts/sessionStorage/login.js
@@ -1,5 +1,22 @@
+import apiManager from "../apiManager";
+
 export default {
-    storeLogin: function(){
-        
+    storeLogin: function (username, email) {
+        let userFound = false;
+        sessionStorage.clear()
+        apiManager.getAll("users").then(response => {
+                for (i = 0; i > response.length; i += 1) {
+                    if (response[i].username === username && response[i].email === email) {
+                        userFound = true;
+                        alert("Logging in...");
+                        sessionStorage.setItem("user", user);
+                        break
+                    }
+                }
+                if (userFound === false){
+                    alert("Login failed. Please check your login credentials.");
+                }
+                location.reload();
+            })
+        }
     }
-}
