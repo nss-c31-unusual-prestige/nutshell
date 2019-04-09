1337dd37de579f38d94ecc3b3b5a6c8ead7b499b
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-09 13:36:10 -0500

    page load on log in feature

--- src/scripts/Homepage/homePage.js
+++ src/scripts/Homepage/homePage.js
@@ -1,6 +1,17 @@
 import htmlBuilder from "../HtmlBuilder.js"
 import homeEvent from "./homeEventListener.js"
 import login from "../sessionStorage/login.js";
+import messageEventHandler from "../chat/messageEventHandler.js";
+import apiManager from "./apiManager.js";
+import newsFeedDOM from "./newsFeed/newsDomManage.js"
+import newsForm from "./newsFeed/newsForm.js"
+import eventsDom from "./events/eventsDomManager.js"
+import eventsForm from "./events/eventsForm.js"
+import taskForm from "./task/taskForm.js"
+import tasksDomBuilder from "./task/tasksDomBuilder.js";
+
+
+
 
 let mainArticle = document.getElementById("home-section")
 let currentUser = sessionStorage.getItem("user");
@@ -12,6 +23,7 @@ export default {
     homePage() {
         let homePageArticle = htmlBuilder.elementBuilder("article", "homePageArticle")
         homePageArticle.appendChild(htmlBuilder.elementBuilder("h1", "homeTitle", "Unusual Homepage"))
+        //when logged in.
         if (currentUser) {
             console.log(currentUser);
             homePageArticle.appendChild(htmlBuilder.elementBuilder("h2", "welcome", `Welcome, ${currentName}.`));
@@ -21,6 +33,17 @@ export default {
                 login.logOut();
             })
             homePageArticle.appendChild(logOutButton);
+            //calls the page functions when the user is active.
+            newsForm.newsForm()
+            newsFeedDOM.childArticleContainer()
+            newsFeedDOM.listAllNews()
+            eventsForm.eventForm()
+            eventsDom.childEventContainer()
+            eventsDom.listAllEvents()
+            taskForm.taskFormSection()
+            tasksDomBuilder.listAllTask()
+            messageEventHandler.createMessages()
+            //when unregistered
         } else {
             homePageArticle.appendChild(htmlBuilder.elementBuilder("label", undefined, "Username"))
             homePageArticle.appendChild(htmlBuilder.elementBuilder("input", "Username", "User"))
