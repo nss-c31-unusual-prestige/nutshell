64600c9a32ec9c17962cedaeb76edb13c2fb4232
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-09 14:25:51 -0500

    Created the dummy data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-import homepage from "./Homepage/homePage.js"
-homepage.homePage()
-
-
-
-
-import apiManager from "./apiManager.js";
-
-import newsFeedDOM from "./newsFeed/newsDomManage.js"
-import newsForm from "./newsFeed/newsForm.js"
-
-newsForm.newsForm()
-newsFeedDOM.childArticleContainer()
-newsFeedDOM.listAllNews()   
-
-import messageHandler from "./chat/messages.js"
-messageHandler.createChat();
-
-import eventsDom from "./events/eventsDomManager.js"
-import eventsForm from "./events/eventsForm.js"
-
-eventsForm.eventForm()
-eventsDom.childEventContainer()
-eventsDom.listAllEvents()
-
-import taskForm from "./task/taskForm.js"
-import tasksDomBuilder from "./task/tasksDomBuilder.js";
-
-
-taskForm.taskFormSection()
-tasksDomBuilder.listAllTask()
-
-=======
 import homepage from "./Homepage/homePage.js"
 
 homepage.homePage()
->>>>>>> ecd43a3fdd6c780ebe925757634891324d8eb885
