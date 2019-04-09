1abb7e632d13fb991c324fada96779305f1cf189
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-09 10:30:09 -0500

    Updating Master

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,14 @@ import apiManager from "./apiManager.js";
 import newsFeedDOM from "./newsFeed/newsDomManage.js"
 import newsForm from "./newsFeed/newsForm.js"
 import newsEventHandler from "./newsFeed/newsEventHandler.js"
-import homepage from "./Homepage/homePage.js"
+// import homepage from "./Homepage/homePage.js"
 
-// newsForm.newsForm()
-// newsFeedDOM.childArticleContainer()
-// newsFeedDOM.listAllNews()
+newsForm.newsForm()
+newsFeedDOM.childArticleContainer()
+newsFeedDOM.listAllNews()
 
-homepage.homePage()
+// homepage.homePage()
 
-import messageHandler from "./chat/messages.js"
+// import messageHandler from "./chat/messages.js"
 
-messageHandler.createChat();
+// messageHandler.createChat();
