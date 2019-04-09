bccd0b8290c18531b5e3aceb81aecfc1dc25652c
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-04-09 14:03:42 -0500

    Checking branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,33 @@
+
+import homepage from "./Homepage/homePage.js"
+homepage.homePage()
+
+
+
+
 import apiManager from "./apiManager.js";
+
 import newsFeedDOM from "./newsFeed/newsDomManage.js"
 import newsForm from "./newsFeed/newsForm.js"
-import newsEventHandler from "./newsFeed/newsEventHandler.js"
-// import homepage from "./Homepage/homePage.js"
 
 newsForm.newsForm()
 newsFeedDOM.childArticleContainer()
-newsFeedDOM.listAllNews()
+newsFeedDOM.listAllNews()   
 
-
-import homepage from "./Homepage/homePage.js"
 import messageHandler from "./chat/messages.js"
-
-
-homepage.homePage()
 messageHandler.createChat();
+
+import eventsDom from "./events/eventsDomManager.js"
+import eventsForm from "./events/eventsForm.js"
+
+eventsForm.eventForm()
+eventsDom.childEventContainer()
+eventsDom.listAllEvents()
+
+import taskForm from "./task/taskForm.js"
+import tasksDomBuilder from "./task/tasksDomBuilder.js";
+
+
+taskForm.taskFormSection()
+tasksDomBuilder.listAllTask()
 
