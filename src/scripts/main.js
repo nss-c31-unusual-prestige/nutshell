94cc3ab20d568dd8df4b934db240d6403c3111ce
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-09 09:58:52 -0500

    fixed problem with local branch not matching stored branch for some reason

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+import apiManager from "./apiManager.js";
+import newsFeedDOM from "./newsFeed/newsDomManage.js"
+import newsForm from "./newsFeed/newsForm.js"
+import newsEventHandler from "./newsFeed/newsEventHandler.js"
+import homepage from "./Homepage/homePage.js"
+import messageHandler from "./chat/messages.js"
+// newsForm.newsForm()
+// newsFeedDOM.childArticleContainer()
+// newsFeedDOM.listAllNews()
+
+homepage.homePage()
+messageHandler.createChat();
