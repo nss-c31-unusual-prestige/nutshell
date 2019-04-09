fcc9e59a6cbbb1075b1a299a904d303fd951a5a4
AliAbdulle <alidhobey@gmail.com>
2019-04-09 13:05:22 -0500

    Saved new section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,7 @@
-
 import taskForm from "./task/taskForm.js"
 import tasksDomBuilder from "./task/tasksDomBuilder.js";
 import taskEventHandler from "./task/taskEventHandler.js";
 
 taskForm.taskFormSection()
 tasksDomBuilder.listAllTask()
-// taskEventHandler.listAllTask()
-import apiManager from "./apiManager.js";
-import newsFeedDOM from "./newsFeed/newsDomManage.js"
-import newsForm from "./newsFeed/newsForm.js"
-import newsEventHandler from "./newsFeed/newsEventHandler.js"
-import homepage from "./Homepage/homePage.js"
-import messageHandler from "./chat/messages.js"
-// newsForm.newsForm()
-// newsFeedDOM.childArticleContainer()
-// newsFeedDOM.listAllNews()
 
-homepage.homePage()
-messageHandler.createChat();
